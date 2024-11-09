6a18a97b6f1a9c2ea26bf4765490936e87397daa
Saull Brandão <saull@outlook.com>
2024-11-09 09:03:11 -0300

    update fish config

---
+++ private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -0,0 +1,19 @@
+# Chezmoi
+abbr -a c chezmoi
+abbr -a ca 'chezmoi add'
+abbr -a cra 'chezmoi re-add'
+abbr -a ccd 'chezmoi cd'
+
+# Git
+abbr -a g git
+abbr -a ga 'git add'
+abbr -a gaa 'git add --all'
+abbr -a gcl 'git clone'
+abbr -a gc 'git commit'
+abbr -a gcm 'git commit -m'
+abbr -a gcam 'git commit -a -m'
+abbr -a gd 'git diff'
+abbr -a gp 'git push'
+abbr -a gs 'git status'
+abbr -a gco 'git checkout'
+abbr -a gcb 'git checkout -b'
